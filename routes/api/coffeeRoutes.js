5a00b995d22b0da02098ecc4530022923bf119f1
Aaron Paden <aaronbpaden@gmail.com>
2022-03-21 15:19:20 -0500

    Add single-product route.

--- routes/api/coffeeRoutes.js
+++ routes/api/coffeeRoutes.js
@@ -18,12 +18,34 @@ const fetchProductsList = (URL, title, name, productType, res) => {
 		});
 };
 
+const fetchProduct = (URL, res) => {
+	fetch(URL)
+		.then(res => res.json())
+		.then(data => {
+			res.render('pages/single-product', {
+				title: `${data.title} page`,
+				name: `Check out this ${data.title}`,
+				data
+			});
+		});
+};
+
 router.get('/hot', (req, res) => {
 	fetchProductsList(`${API}/hot`, 'Buy Hot Coffee', 'Check out our hot coffee listing', 'hot', res);
 });
 
 router.get('/iced', (req, res) => {
 	fetchProductsList(`${API}/iced`, 'Buy Iced Coffee', 'Check out our iced coffee listing', 'iced', res);
+});
+
+router.get('/hot/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	fetchProduct(`${API}/hot/${id}`, res);
+});
+
+router.get('/iced/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	fetchProduct(`${API}/iced/${id}`, res);
 });
 
 module.exports = router;
